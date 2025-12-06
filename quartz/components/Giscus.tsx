001f31363b64cb0ed832d36c799a4aa65eb09a39
Dongmin Lee <fftl0785@gmail.com>
2025-12-06 21:44:25 +0900

    Quartz sync: Dec 6, 2025, 9:44 PM

--- quartz/components/Giscus.tsx
+++ quartz/components/Giscus.tsx
@@ -4,23 +4,21 @@ export default (() => {
   const Giscus: QuartzComponent = ({ displayClass }: QuartzComponentProps) => {
     return (
       <div class={`giscus ${displayClass ?? ""}`}>
-        <script
-          src="https://giscus.app/client.js"
-          data-repo="username/username.github.io"  // 본인 저장소로 변경
-          data-repo-id="YOUR_REPO_ID"  // giscus.app에서 생성된 값
-          data-category="General"
-          data-category-id="YOUR_CATEGORY_ID"  // giscus.app에서 생성된 값
-          data-mapping="pathname"
-          data-strict="0"
-          data-reactions-enabled="1"
-          data-emit-metadata="0"
-          data-input-position="bottom"
-          data-theme="preferred_color_scheme"
-          data-lang="ko"
-          data-loading="lazy"
-          crossorigin="anonymous"
-          async
-        ></script>
+        <script src="https://giscus.app/client.js"
+        data-repo="fftl/fftl.github.io"
+        data-repo-id="R_kgDOQdGjkA"
+        data-category="General"
+        data-category-id="DIC_kwDOQdGjkM4CzeDn"
+        data-mapping="pathname"
+        data-strict="0"
+        data-reactions-enabled="1"
+        data-emit-metadata="1"
+        data-input-position="bottom"
+        data-theme="preferred_color_scheme"
+        data-lang="ko"
+        crossorigin="anonymous"
+        async>
+</script>
       </div>
     )
   }
